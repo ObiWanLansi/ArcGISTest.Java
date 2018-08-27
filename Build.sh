35e51bfce188a57b6a33ada42895ced702cf3aab
Jörg Lanser <38795604+ObiWanLansi@users.noreply.github.com>
2018-08-27 16:55:12 +0200

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#java -cp . -jar ./lib/lansiinformationcenter.jar lansi.lic.application.LansiInformationCenter
-
-#java.exe -Djava.ext.dirs=.\lib; -jar .\lib\lansiinformationcenter.jar %*
-
-#java -classpath ./lib/fluent-hc-4.5.2.jar:./lib/httpclient-4.5.2.jar -jar ./lib/lansiinformationcenter.jar &&
-
-
-#java -cp ./lib/commons-logging-1.2.jar:./lib/fluent-hc-4.5.2.jar:./lib/httpclient-4.5.2.jar:./lib/httpcore-4.4.4.jar  -jar ./lib/lansiinformationcenter.jar &
-
 export JDK_HOME=/opt/jdk1.8.0_181
 export JAVA_HOME=/opt/jdk1.8.0_181
 export PATH=/opt/jdk1.8.0_181/bin:/opt/jdk1.8.0_181/jre/bin:$PATH
